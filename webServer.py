1c390782302df05a4e83167ac14804bd296bd3c5
lihongyuan <hy.li@8win.com>
2016-08-14 17:25:34 +0800

    format code

--- webServer.py
+++ webServer.py
@@ -10,23 +10,29 @@ from util.rrdcontroller import RRDController
 import os
 import time
 import json
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
+
 def updateRRD():
     pass
+
 
 def graphImage():
     pass
 
+
 @app.route('/upload/', methods=['POST'])
+
 def utilData():
     data = request.get_data()
     data = json.loads(data)
-	# return render_template('index.html',data=data)
+    # return render_template('index.html',data=data)
     ipAddress = str(data["ipaddress"])
     serviceName = str(data["Process_Name"])
     jvmType = str(data["jvmType"])
@@ -35,7 +41,7 @@ def utilData():
     staticFile = '/data/apps/jvm_monitor/%s/%s/%s.png' % (
         ipAddress, serviceName, jvmType)
     print "variable type"
-    print type(rrdFile),type(staticFile)
+    print type(rrdFile), type(staticFile)
     print "------------------------"
     rrd = RRDController(rrdfile=rrdFile, static_path=staticFile)
     if os.path.exists(os.path.dirname(rrdFile)) is not True:
@@ -48,7 +54,17 @@ def utilData():
     if os.path.isfile(rrdFile) is not True:
         rrd = rrd.create()
     print "recv data success"
-    result = rrd.update(rrdfile=rrdFile,YGCT_avg=float(data["YGCT_avg"]),Heap_used=float(data['Heap_used']),S0_used=float(data['S0_used']),S0_max=float(data['S0_max']),S0_ratio=float(data['S0_ratio']),S1_used=float(data['S1_used']),S1_max=float(data['S1_max']),S1_ratio=float(data['S1_ratio']),Old_used=float(data['Old_used']),Old_max=float(data['Old_max']),Old_ratio=float(data['Old_ratio']),Eden_used=float(data['Eden_used']),Eden_max=float(data['Eden_max']),Eden_ratio=float(data['Eden_ratio']),Metadata_used=float(data['Metadata_used']),Metadata_max=float(data['Metadata_max']),Metadata_ratio=float(data['Metadata_ratio']),Heap_max=float(data['Heap_max']),Heap_ratio=float(data['Heap_ratio']),YGC=float(data['YGC']),FGC=float(data['FGC']),YGCT=float(data['YGCT']),FGCT=float(data['FGCT']),GCT=float(data['GCT']),FGCT_avg=float(data['FGCT_avg']),GCT_avg=float(data['GCT_avg']))
+    result = rrd.update(rrdfile=rrdFile, YGCT_avg=float(data["YGCT_avg"]), Heap_used=float(data['Heap_used']),
+                        S0_used=float(data['S0_used']), S0_max=float(data['S0_max']), S0_ratio=float(data['S0_ratio']),
+                        S1_used=float(data['S1_used']), S1_max=float(data['S1_max']), S1_ratio=float(data['S1_ratio']),
+                        Old_used=float(data['Old_used']), Old_max=float(data['Old_max']),
+                        Old_ratio=float(data['Old_ratio']), Eden_used=float(data['Eden_used']),
+                        Eden_max=float(data['Eden_max']), Eden_ratio=float(data['Eden_ratio']),
+                        Metadata_used=float(data['Metadata_used']), Metadata_max=float(data['Metadata_max']),
+                        Metadata_ratio=float(data['Metadata_ratio']), Heap_max=float(data['Heap_max']),
+                        Heap_ratio=float(data['Heap_ratio']), YGC=float(data['YGC']), FGC=float(data['FGC']),
+                        YGCT=float(data['YGCT']), FGCT=float(data['FGCT']), GCT=float(data['GCT']),
+                        FGCT_avg=float(data['FGCT_avg']), GCT_avg=float(data['GCT_avg']))
 
     if result['return_value'] == 0L:
         print "success update value"
@@ -58,6 +74,7 @@ def utilData():
     rrd.graphJavaS0S1EdenMetadata()
 
     return "graph success"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
